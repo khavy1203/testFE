974de6d6d44cc14ed5f799d7d04fb736895beec4
unknown <dh51704561@student.stu.edu.vn>
2022-10-15 08:33:13 +0700

    60%

--- src/component/Content/Content.js
+++ src/component/Content/Content.js
@@ -1,10 +1,91 @@
-import './Content.css'
+import "./Content.css";
 const Content = () => {
-    return (
-        <>
-        
+  return (
+    <>
+      <div className="header">
+        <div className="img-logo"></div>
+      </div>
+      <div className="center">
+        <div className="content-center">
+          <div className="top">
+            <p className="header-para"> Lorem ipsum dolor sit asmet?</p>
 
-        </>
-    )
-}
+            <p className="body-para">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi
+              suscipit nam veritatis animi voluptate vitae, sunt in magnam. Quis
+              obcaecati, neque aspernatur voluptatem quaerat dolore minima magni
+              voluptatibus ipsum officia!Lorem ipsum dolor sit, amet consectetur
+              adipisicing elit. Commodi suscipit nam veritatis animi voluptate
+              vitae, sunt in magnam. Quis obcaecati, neque aspernatur voluptatem
+              quaerat dolore minima magni voluptatibus ipsum officia!Lorem ipsum
+              dolor sit, amet consectetur adipisicing elit. Commodi suscipit nam
+              veritatis animi voluptate vitae, sunt in magnam. Quis obcaecati,
+              neque aspernatur voluptatem quaerat dolore minima magni
+              voluptatibus ipsum officia!
+            </p>
+          </div>
+        </div>
+        <div className="down">
+          <div className="item-down">
+            <div className="item-down-header">
+              <h5>
+                Lorem ipsum dolor sit amet consectetur
+              </h5>
+            </div>  
+            <div className="item-down-body">
+                <div className="hinh-anh-1"></div>
+                <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi
+              suscipit nam veritatis animi voluptate vitae, sunt in magnam. Quis
+              obcaecati, neque aspernatur voluptatem quaerat dolore minima magni
+              voluptatibus ipsum officia!Lorem ipsum dolor sit, amet consectetur
+              adipisicing elit. Commodi suscipit nam veritatis animi voluptate
+              vitae, sunt in magnam. Quis obcaecati, 
+            </p>
+            </div>
+            
+          </div>
+          <div className="item-down">
+            <div className="item-down-header">
+              <h5>
+                Lorem ipsum dolor sit amet consectetur
+              </h5>
+            </div>  
+            <div className="item-down-body">
+                <div className="hinh-anh-1"></div>
+                <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi
+              suscipit nam veritatis animi voluptate vitae, sunt in magnam. Quis
+              obcaecati, neque aspernatur voluptatem quaerat dolore minima magni
+              voluptatibus ipsum officia!Lorem ipsum dolor sit, amet consectetur
+              adipisicing elit. Commodi suscipit nam veritatis animi voluptate
+              vitae, sunt in magnam. Quis obcaecati, 
+            </p>
+            </div>
+            
+          </div>
+          <div className="item-down">
+            <div className="item-down-header">
+              <h5>
+                Lorem ipsum dolor sit amet consectetur
+              </h5>
+            </div>  
+            <div className="item-down-body">
+                <div className="hinh-anh-1"></div>
+                <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi
+              suscipit nam veritatis animi voluptate vitae, sunt in magnam. Quis
+              obcaecati, neque aspernatur voluptatem quaerat dolore minima magni
+              voluptatibus ipsum officia!Lorem ipsum dolor sit, amet consectetur
+              adipisicing elit. Commodi suscipit nam veritatis animi voluptate
+              vitae, sunt in magnam. Quis obcaecati, 
+            </p>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default Content;
